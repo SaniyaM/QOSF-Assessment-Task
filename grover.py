d585ae20faab3b36ff30ce0f1ee4196e46220a5c
SaniyaM <31589049+SaniyaM@users.noreply.github.com>
2021-09-28 23:19:23 +0530

    Update grover.py

--- grover.py
+++ grover.py
@@ -15,14 +15,14 @@ import os
 from functions_qosf import clause_matrix_0, solution_states, binarize, inversion, sat, clause_matrix_1, entangle, init_vector
 import subprocess
 
-if not os.path.isdir("./QOSF")
+if not os.path.isdir("./QOSF"):
     subprocess.run("mkdir QOSF", shell = True)
 #array_i = [1, 2, 3, 4] #input array
 #array_i = list(np.random.randint(low = 0, high = 7, size = 4, dtype = np.int64))
 
 
 if __name__ == "__main__":
-	enter = """\nEnter input array"""
+	enter = """\nEnter input array\n"""
 	input_array = [int(i) for i in input(enter).split()]
     
 array_i = input_array #input array
